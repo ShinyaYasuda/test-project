7ae89742262caf54a6d71228d38dfd0f3cbe84c6
ShinyaYasuda <yasuda.shinya@sakura-computer.co.jp>
2022-01-06 20:08:12 +0900

    2022/01/06

--- pages/sub/document.vue
+++ pages/sub/document.vue
@@ -1,5 +1,31 @@
 <template>
     <div>
         <p>sub document file</p>
+        <!-- <v-text-field label="text search" v-model="query"></v-text-field> -->
+        <v-card v-for="post in posts" :key="post.id">
+            <v-card-title>{{ post.title }}</v-card-title>
+            <v-card-subtitle>{{ post.userId }}</v-card-subtitle>
+            <v-card-text>{{ post.body }}</v-card-text>
+        </v-card>
     </div>
 </template>
+<script>
+import axios from 'axios'
+export default ({
+    data: () => ({
+        posts: {
+            body : "",
+            id : 0,
+            title : "",
+            userId : 0
+        }
+    }),
+    async asyncData() {
+        const result = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        console.log(result.data)
+        return {
+            posts: result.data
+        }
+  }
+})
+</script>
